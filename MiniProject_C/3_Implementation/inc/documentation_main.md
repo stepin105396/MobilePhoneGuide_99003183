d8c3601ef0ca6e03e64d72618e4978b78e1b0e8b
Akshay Sai Reddy G <69197931+stepin105396@users.noreply.github.com>
2020-11-14 07:46:36 +0530

    Add files via upload

---
+++ MiniProject_C/3_Implementation/inc/documentation_main.md
@@ -0,0 +1,2 @@
+@mainpage Mobile Phone Guide by Akshay Sai Reddy G
+@subpage my_project.h
---
+++ MiniProject_C/3_Implementation/inc/my_project.h
@@ -0,0 +1,41 @@
+/**
+* @file my_project.h
+*
+*/
+#ifndef __MY_PPROJECT_H__
+#define __MY_PPROJECT_H__
+
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+
+/**
+* Checks if the entered login credentials are correct
+* @param[in] nm
+* @param[in] pswrd
+* @return 1 if correct and 0 if either of them are wrong
+*/
+int admin();
+
+/**
+* Checks if there is any mob8ile with a particular processor p
+* @param[in] p
+* @return Mobile name if found
+*/
+char *a_mobiledetails(int p);
+
+/**
+* Checks if there are any mobiles from the brand
+* @param[in] brand
+* @return number of mobiles from the brand
+*/
+int showdeviceno(char brand[10]);
+
+/**
+* Check the number of mobiles having battery capacity >= b
+* @param[in] b
+* @return number of mobiles with battery capacity >= b
+*/
+int devicebattery(int b);
+
+#endif /* #ifndef __MY_PPROJECT_H__ */
